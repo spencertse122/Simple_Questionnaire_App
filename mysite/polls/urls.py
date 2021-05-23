ef42142b56d6909e24ebfc9219dcb0a9a251e935
Spencer Tse <spencertse122@gmail.com>
2021-05-23 16:33:36 -0400

    only made small progress

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
@@ -13,4 +15,6 @@ urlpatterns = [
     path('<int:question_id>/vote/', views.vote, name='vote'),
     # exercise: trying out to add another view using the shortcut render
     path('index2/', views.index2, name='index2'),
+    # added the word 'specifics'
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
 ]
